9476f6dc6ff3481b017e43b859208ebb9ead710f
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2026-01-08 16:01:26 -0800

    Update dependencies

---
+++ src/server/db/kvdex.ts
@@ -0,0 +1,11 @@
+// TODO: Implement kvdex for the Worlds API system.
+// https://github.com/oliver-oloughlin/kvdex?tab=readme-ov-file#collection-options
+//
+
+// import { collection, kvdex, model } from "@olli/kvdex";
+
+// export const kv = await Deno.openKv();
+
+// export const db = kvdex({
+//   kv: kv,
+// });
